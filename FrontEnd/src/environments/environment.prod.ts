2581675172ba5be2b7e4302e8f49b45e599cb986
Keerthana Devi <90337046+KeerthanaDevi09@users.noreply.github.com>
2023-09-25 12:50:36 +0530

    Add files via upload

---
+++ FrontEnd/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl1:"http://localhost:8085/employee/",
+  baseUrl2:"http://localhost:8085/customer/",
+  baseUrl3:"http://localhost:8085/bill",
+  baseUrl4:"http://localhost:8085/transaction"
+};
